fa889cae93b0e526935b948973edd02c39109e18
Aagamj997 <80820614+Aagamj997@users.noreply.github.com>
2022-07-04 16:18:44 +0400

    Add files via upload

---
+++ grading_sys.py
@@ -0,0 +1,20 @@
+print("This Program Helps You Fing Your Grade By Your Marks")
+a=float(input("Enter Your Marks:\n"))
+if (a>=100):
+    print(a," Falls Under A+")
+elif(a>=90):
+    print(a," Falls Under A")
+elif(a>=80):
+    print(a," Falls Under B+")
+elif(a>=70):
+    print(a," Falls Under B")
+elif(a>=60):
+    print(a," Falls Under C+")
+elif(a>=50):
+    print(a," Falls Under C")
+elif(a>=40):
+    print(a," Falls Under D+")
+elif(a>=30):
+    print(a," Falls Under D")
+elif(a<30):
+    print("Bhaii Padhlo Jake")
