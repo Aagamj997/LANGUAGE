33e22608e3c33397c93260b61cf6dcd5b06999dc
Aagamj997 <80820614+Aagamj997@users.noreply.github.com>
2022-07-08 10:13:03 +0530

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,6 @@
+print("This Program Tells if You Are Eligible For Voting")
+a=int(input("Enter Your Age :\n"))
+if (a>=18):
+    print("Your Can Vote")
+else :
+    print("You Are Not Eligible Yet")
