fe047f236e1545fd81c669ba7a8f65f1e3cf6fc6
Aagam Jain <80820614+Aagamj997@users.noreply.github.com>
2022-08-04 22:05:41 +0530

    Add files via upload

---
+++ aj_buzznocheck.java
@@ -0,0 +1,12 @@
+public class aj_buzznocheck{
+    public static void main(int b){
+        if (b%7==0 || b%10==7)
+        {
+            System.out.println(b+"is a buzz number");
+        }
+        else
+        {
+            System.out.println(b+"is not a buzz number");
+        }
+    }
+}
